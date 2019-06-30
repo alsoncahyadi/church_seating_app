65992efd4351b4dcfd2bb2d33b09960ef872e5a8
Alson Cahyadi <alson.cahyadi@gmail.com>
2019-06-30 13:14:07 +0700

    Edited consumer now using jsonconsumers

--- seating/consumers.py
+++ seating/consumers.py
@@ -2,19 +2,14 @@ import asyncio
 import json
 from django.contrib.auth import get_user_model
 from channels.consumer import AsyncConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 
 # from .models import Thread, ChatMessage
 
-class SeatConsumer(AsyncConsumer):
-    async def websocket_connect(self, event):
-        print('connected', event)
-        await self.send({
-            'type': 'websocket.accept'
+class SeatConsumer(AsyncJsonWebsocketConsumer):
+    async def receive_json(self, content):
+        print("received json:", content)
+        await self.send_json({
+            'data': 'Okay'
         })
-
-    async def websocket_receive(self, event):
-        print('received', event)
-    
-    async def websocket_disconnect(self, event):
-        print('disconnected', event)
