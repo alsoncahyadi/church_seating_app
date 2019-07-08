19a018a54489add059b5a42ab5c3239727fe0660
Alson Cahyadi <alson.cahyadi@gmail.com>
2019-07-08 22:18:20 +0700

    Updated whitenoise

--- seating_site/settings.py
+++ seating_site/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 # Channels
 ASGI_APPLICATION = 'seating_site.routing.application'
